56017525036c1b014416405d9eb22a1bc5d99803
Rohit <ro8267856984@gmail.com>
2025-08-10 17:12:57 +0530

    Complete delete user listing

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -8,3 +8,19 @@ export const createListing=async (req, res, next) => {
         next(error);
     }
 }
+
+export const deleteListing=async (req, res, next) => {
+    const listing=await Listing.findById(req.params.id);
+    if(!listing){
+        return next(new Error('Listing not found'));
+    }
+    if(req.user.id !== listing.userRef){
+        return next(new Error('You can only delete your own listings!'));
+    }
+    try{
+        await Listing.findByIdAndDelete(req.params.id);
+        return res.status(200).json("Listing deleted successfully!");
+    }catch(error){
+        next(error);
+    }
+}
